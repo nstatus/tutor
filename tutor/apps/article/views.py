7ddca219c0f12f5817b36a032612213880ec5349
sergey <sergey.sipiev@yandex.ru>
2015-06-29 21:26:02 +0300

    added Cookies, Session

--- tutor/apps/article/views.py
+++ tutor/apps/article/views.py
@@ -7,12 +7,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from forms import CommentForm
 from django.core.context_processors import csrf
 
-# Create your views here.
 
 def basic_one(request):
     view = 'basic_one'
     html = '<html><body>This is %s view.</body></html>' % view
     return HttpResponse(html)
+
 
 def template_two(request):
     view = 'template_two'
@@ -20,13 +20,16 @@ def template_two(request):
     html = t.render(Context({'name': view}))
     return HttpResponse(html)
 
+
 def template_three_simple(request):
     view = 'template_three_simple'
     return render_to_response('myview.html', {'name': view})
 
+
 def articles(request):
     return render_to_response('articles.html',
                               {'articles': Article.objects.all().order_by('article_date')})
+
 
 def article(request, article_id=1):
     comments_form = CommentForm
@@ -40,20 +43,30 @@ def article(request, article_id=1):
     #                           {'article':Article.objects.get(id=article_id),
     #                            'comments': Comments.objects.filter(comments_article_id=article_id)})
 
+
 def addlike(request, article_id):
     try:
-        article = Article.objects.get(id=article_id)
-        article.article_likes += 1
-        article.save()
+        if article_id in request.COOKIES:
+            return redirect('/')
+        else:
+            article = Article.objects.get(id=article_id)
+            article.article_likes += 1
+            article.save()
+            response = redirect('/')
+            response.set_cookie(article_id, 'test')
+            return response
     except ObjectDoesNotExist:
         raise Http404
-    return redirect('/')
+    # return redirect('/')
+
 
 def addcomment(request, article_id):
-    if request.POST:
+    if request.POST and ('pause' not in request.session):
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.comments_article = Article.objects.get(id=article_id)
             form.save()
+            request.session.set_expiry(60)
+            request.session['pause'] = True
     return redirect('/articles/get/%s/' % article_id)
